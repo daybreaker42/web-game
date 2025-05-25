a14f30f00938fbc23f6cdbffb2d8f87837319093
yonaMac <yonaimyo@gmail.com>
2025-05-25 20:11:38 +0900

    Fix: title animation flicker

--- src/script.js
+++ src/script.js
@@ -494,21 +494,49 @@ btnNo.onclick = () => {
 
 let cloudAnimTimer = null;
 
+function preloadCloudFrames(frameCount, basePath) {
+  const frames = [];
+  let loadedCount = 0;
+  return new Promise((resolve) => {
+    for (let i = 0; i < frameCount; i++) {
+      const img = new Image();
+      img.src = `${basePath}${i}.png`;
+      img.onload = () => {
+        frames[i] = img;
+        loadedCount++;
+        if (loadedCount === frameCount) resolve(frames);
+      };
+      img.onerror = () => {
+        // 실패해도 비워둠
+        loadedCount++;
+        if (loadedCount === frameCount) resolve(frames);
+      };
+    }
+  });
+}
+
 function startCloudAnimation() {
   const root = document.documentElement;
-  const frameCount = 3;
+  const frameCount = 4;
+  const basePath = "../assets/images/background/title_";
   let frameIdx = 0;
+  let frames = [];
 
-  const img = new Image();
-  img.src = `../assets/images/background/title_0.png`;
-  img.onload = () => {
-    root.style.setProperty("--img-menu-bg", `url('${img.src}')`);
+  // 프레임 preload 후 애니메이션 시작
+  preloadCloudFrames(frameCount, basePath).then((loadedFrames) => {
+    frames = loadedFrames;
+    // 첫 프레임 강제 표시
+    if (frames[0] && frames[0].src) {
+      root.style.setProperty("--img-menu-bg", `url('${frames[0].src}')`);
+    }
     cloudAnimTimer = setInterval(() => {
-      const url = `url('../assets/images/background/title_${frameIdx}.png')`;
-      root.style.setProperty("--img-menu-bg", url);
+      const img = frames[frameIdx];
+      if (img && img.src) {
+        root.style.setProperty("--img-menu-bg", `url('${img.src}')`);
+      }
       frameIdx = (frameIdx + 1) % frameCount;
-    }, 400);
-  };
+    }, 500);
+  });
 }
 
 function stopCloudAnimation() {
