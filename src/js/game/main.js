bdffb151578daaf1d7569baf8265d9c990601c90
한성준 <june123000@naver.com>
2025-05-29 12:39:07 +0900

    game/main.js - debug_mode undefined fix

--- src/js/game/main.js
+++ src/js/game/main.js
@@ -1,6 +1,6 @@
 // 페이지 로드 시 초기화
 document.addEventListener("DOMContentLoaded", function () {
-  if (DEBUG_MODE) {
+  if (window.DEBUG_MODE) {
     switchGameMode("brick"); // 기본 모드로 초기화
   }
   // 게임 컨트롤 버튼 이벤트 리스너 설정 (한 번만) // 주석 추가
