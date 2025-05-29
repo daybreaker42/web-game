930a779dc4a5a3cc91fa422f3e7f9954e7e7ecb2
yonaMac <yonaimyo@gmail.com>
2025-05-29 14:27:24 +0900

    Refactor: stageIdx using constnat

--- src/js/stage-manager.js
+++ src/js/stage-manager.js
@@ -4,17 +4,16 @@ function proceedToStage(stageIdx) {
     currentStageIndex = stageIdx;
     console.log(`Proceeding to stage ${stageIdx}`);
     
+    // 모든 스테이지 완료 - ENDING 스토리 재생
     if (stageIdx > N_STAGES) {
-      // 모든 스테이지 완료 - ENDING 스토리 재생
-      playStory(5, () => { // story_chapter5_closing
+      playStory(N_STAGES + 1, () => { // story_chapter5_closing
         onAllStagesCleared();
       });
       return;
     }
     
     // 각 스테이지 전 스토리 재생
-    playStory(stageIdx, () => { // stage 1이면 story_chapter1
-      // 스토리 후 게임 시작
+    playStory(stageIdx, () => {
       playGame("story", selectedDifficulty, stageIdx, (gameResult) => {
         onGameEnd(gameResult);
       });
@@ -26,12 +25,11 @@ function proceedToStage(stageIdx) {
  * 게임 끝나고 호출하는 함수, 다음 스테이지로 넘어가거나 없으면 타이틀로
  */
 function onGameEnd(gameResult) {
-    saveGameResult(gameResult);
-    hideWithFade(qs("#gameplay-screen"));
-    
     if (gameResult.game_over) {
       onStageOver(gameResult);
     } else {
+    saveGameResult(gameResult);
+    hideWithFade(qs("#gameplay-screen"));    
       onStageClear(gameResult);
     }
   }
@@ -41,10 +39,8 @@ function onGameEnd(gameResult) {
 // =============================================================================
 
 function onAllStagesCleared() {
-    alert("축하합니다. 모든 스테이지를 클리어했습니다!\n타이틀로 돌아갑니다.");
-    
     // ENDING 스토리 재생
-    playStory(5, () => { // story_chapter5_closing
+    playStory(N_STAGES + 1, () => { // story_chapter5_closing
       showCredits();
       handleReturnToTitleScreen();
     });
@@ -53,8 +49,8 @@ function onAllStagesCleared() {
   function onStageClear(gameResult) {
     alert(`Stage ${currentStageIndex}를 클리어했습니다.`);
     
-    if (currentStageIndex === 4) {
-      // Stage 4 클리어 후 바로 엔딩
+    if (currentStageIndex === N_STAGES) {
+      // Stage 4 클리어
       onAllStagesCleared();
     } else {
       // Stage 1~3 클리어 후 해당 스토리 재생
